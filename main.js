608c869a28286166bb2b2e5a632b009803e3b273
Ilya Morozov <finnlivefx1@mail.ru>
2023-03-20 21:05:49 +0700

    Added function search more images w/o updating

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 
 input.addEventListener('keydown', function (event) {
-	if (event.key === 'Enter') loadImg()
+	if (event.key === 'Enter') loaderImg()
 })
 
 function loadImg() {
@@ -15,7 +15,54 @@ function loadImg() {
 	const url =
 		'https://api.unsplash.com/photos/random/?query=' +
 		input.value +
-		'&count=50&client_id=rnJocnRoW2yp7l8IjAExsm1Jd6x0XdSEINAp4aPj5fE'
+		'&count=50&client_id=U2VKzEnY4GaZyw5hCMwOWcRuGU0bA-VevuCa7OMnemM'
+
+	fetch(url)
+		.then(response => {
+			if (response.ok) return response.json()
+			else alert('Failed to load pictures')
+		})
+		.then(data => {
+			for (let i = 0; i < data.length; i++) {
+				imageNodes.push(document.createElement('div'))
+				imageNodes[imageNodes.length - 1].className = 'img'
+				imageNodes[
+					imageNodes.length - 1
+				].style.backgroundImage = `url(${data[i].urls.regular})`
+				const downloadButton = document.createElement('button')
+				downloadButton.className = 'download-button'
+				const downloadIcon = document.createElement('img')
+				downloadIcon.src = 'public/images/pin/download.svg'
+				downloadButton.appendChild(downloadIcon)
+				imageNodes[imageNodes.length - 1].appendChild(downloadButton)
+				imageNodes[imageNodes.length - 1].addEventListener(
+					'mouseenter',
+					function () {
+						downloadButton.style.display = 'block'
+					}
+				)
+				imageNodes[imageNodes.length - 1].addEventListener(
+					'mouseleave',
+					function () {
+						downloadButton.style.display = 'none'
+					}
+				)
+				downloadButton.addEventListener('click', function () {
+					window.open(data[i].links.download, '_blank')
+				})
+				content.appendChild(imageNodes[imageNodes.length - 1])
+			}
+		})
+}
+
+function loaderImg() {
+	content.innerHTML = ''
+	imageNodes.length = 0
+	event.preventDefault()
+	const url =
+		'https://api.unsplash.com/photos/random/?query=' +
+		input.value +
+		'&count=50&client_id=U2VKzEnY4GaZyw5hCMwOWcRuGU0bA-VevuCa7OMnemM'
 
 	fetch(url)
 		.then(response => {
